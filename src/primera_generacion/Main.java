815e882c951cf5ca518cab07cf3bebf11a066c23
elmaestrotic <elmaestrotic@gmail.com>
2025-03-18 06:55:17 -0500

    Initial commit

---
+++ src/primera_generacion/Main.java
@@ -0,0 +1,10 @@
+import segunda_generacion.Drácula;
+
+public class Main {
+    public static void main(String[] args) {
+        Drácula draculin = new Drácula();
+        draculin.morder();
+        draculin.chuparSangre();
+        draculin.aguantarSol();
+    }
+}
